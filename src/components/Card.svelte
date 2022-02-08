056e535fe5475436b158d6b37d6edd9cf8898a2b
AlexanderB92 <a.m.brandstrup@gmail.com>
2022-02-08 20:47:38 +0100

    some minor changes t o cards, working in griditem, removing feedback components

--- src/components/Card.svelte
+++ src/components/Card.svelte
@@ -4,8 +4,11 @@
 
 <style>
     .card {
-        background-color: white;
-        color: #333;
+        background-color: rgb(255, 167, 36);
+        background-image: url("/assets/images/cardbackground.png");
+        background-repeat: no-repeat;
+        background-size: auto;
+        color: rgb(250, 250, 250);
         border-radius: 15px;
         padding: 40px 50px;
         margin: 15px 0;
